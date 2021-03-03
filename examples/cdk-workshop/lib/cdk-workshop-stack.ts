70118b5e58f2f50746d3c6352217e2629ba10a54
Charlie Pence <cp@thunkabout.it>
2021-03-02 20:05:08 -0500

    initial lambda function

--- examples/cdk-workshop/lib/cdk-workshop-stack.ts
+++ examples/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
+        const hello = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
+        });
 
-  }
+        
+    }
 }
